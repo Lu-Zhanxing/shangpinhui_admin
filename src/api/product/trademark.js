f8e0c336a1a73a6d7405d6c86f9f2d00ed62ae40
Lu-Zhanxing <18330259691@qq.com>
2023-02-04 14:47:27 +0800

    添加和编辑品牌操作

--- src/api/product/trademark.js
+++ src/api/product/trademark.js
@@ -1,8 +1,30 @@
 import request from '@/utils/request.js'
 
+// 品牌列表
 export const reqTradeMarkList = (page,limit) => {
     return request({
         url:`/admin/product/baseTrademark/${page}/${limit}`,
         method:'get'
     })
 }
+
+// 添加或者编辑品牌信息
+export const addOrEditTrademark = (data) => {
+    // 如果传了id,那么走编辑的请求
+    if(data.id){
+        return request({
+            url:`/admin/product/baseTrademark/update`,
+            method:'put',
+            data: data
+        })
+    }else{
+        return request({
+            url:`/admin/product/baseTrademark/save`,
+            method:'post',
+            data: data
+        })
+    }
+}
+
+// 删除品牌
+export const delTrademark = (id) => request({url:`/admin/product/baseTrademark/remove/${id}`,method:'delete'})
--- src/views/product/tradeMark/index.vue
+++ src/views/product/tradeMark/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <el-button type="primary" class="el-icon-plus" style="margin: 10px 0"
+    <el-button
+      type="primary"
+      class="el-icon-plus"
+      style="margin: 10px 0"
+      @click="showTrademarkDialog"
       >添加</el-button
     >
 
@@ -11,13 +15,17 @@
       </el-table-column>
       <el-table-column label="品牌LOGO" width="width">
         <template slot-scope="{ row, $index }">
-          <img style="width:50px;height:50px" :src="row.logoUrl" />
+          <img style="width: 50px; height: 50px" :src="row.logoUrl" />
         </template>
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{ row, $index }">
-          <el-button type="warning" class="el-icon-edit" size="mini">编辑</el-button>
-          <el-button type="danger" class="el-icon-delete" size="mini">删除</el-button>
+          <el-button type="warning" class="el-icon-edit" size="mini" @click="editTrademark(row)"
+            >编辑</el-button
+          >
+          <el-button type="danger" class="el-icon-delete" size="mini" @click="delTrademark(row.id)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -36,6 +44,36 @@
       layout="prev, pager, next, jumper, -> , sizes, total"
     >
     </el-pagination>
+    <!-- 添加、修改品牌弹框 -->
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+      <el-form :model="tmForm" style="width: 80%">
+        <el-form-item
+          label="品牌名称"
+          :label-width="formLabelWidth"
+        >
+          <el-input v-model="tmForm.tmName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" :label-width="formLabelWidth">
+          <el-upload
+            class="avatar-uploader"
+            action="/dev-api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="tmForm.logoUrl" :src="tmForm.logoUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancelEvent">取 消</el-button>
+        <el-button type="primary" @click="addOrEditTrademark">确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -48,6 +86,14 @@ export default {
       limit: 3,
       total: 90,
       list: [],
+      // 弹框相关
+      dialogFormVisible: false,
+      formLabelWidth: "120px",
+      tmForm: {
+        id: '',
+        tmName: '',
+        logoUrl: ''
+      },
     };
   },
   mounted() {
@@ -58,7 +104,6 @@ export default {
     async getTradeMarkList() {
       let { page, limit } = this;
       let result = await this.$API.trademark.reqTradeMarkList(page, limit);
-      console.log(result);
       if (result.code == 200) {
         this.total = result.data.total;
         this.list = result.data.records;
@@ -74,9 +119,95 @@ export default {
       this.page = val;
       this.getTradeMarkList();
     },
+    // 添加品牌弹框
+    showTrademarkDialog() {
+      this.dialogFormVisible = true;
+      this.tmForm.tmName = '';
+      this.tmForm.logoUrl = '';
+    },
+    // 编辑品牌
+    editTrademark(row){
+      this.dialogFormVisible = true;
+      // 方法一：这样写不会造成当编辑弹框品牌的时候，列表中的值也会跟着改，取消按钮无效的问题
+      // this.tmForm.id = row.id;
+      // this.tmForm.tmName = row.tmName;
+      // this.tmForm.logoUrl = row.logoUrl;
+
+      // 方法二：直接赋值的方法
+      // 注意：这样写就不行，会造成双向数据绑定，当编辑弹框品牌的时候，列表中的值也会跟着改的问题
+      // this.tmForm = row
+
+      // 解决：浅拷贝，只拷贝一层，这样就解决了上边这个问题
+      // this.tmForm = {...row}
+      this.tmForm = Object.assign({},row)
+    },
+    // 弹窗取消按钮
+    cancelEvent(){
+      this.dialogFormVisible = false;
+      this.tmForm = {id:'', tmName:'', logoUrl:''}
+    },
+    // 弹窗点击确定按钮
+    async addOrEditTrademark(){
+      this.dialogFormVisible = false;
+      let result = await this.$API.trademark.addOrEditTrademark(this.tmForm)
+      if(result.code == 200){
+        this.$message({
+          message: this.tmForm.id?'修改品牌成功':'添加品牌成功',
+          type: 'success'
+        });
+        this.getTradeMarkList();   
+      }
+         
+    },
+    // 图片上传
+    handleAvatarSuccess(res, file) {
+      // this.tmForm.logoUrl = URL.createObjectURL(file.response.data);
+      this.tmForm.logoUrl = res.data
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    // 删除品牌
+    async delTrademark(rowId){
+      let result = await this.$API.trademark.delTrademark(rowId)
+      console.log(result);
+      this.getTradeMarkList();
+    }
   },
 };
 </script>
 
 <style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
